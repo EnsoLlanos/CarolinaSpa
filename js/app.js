0cd6c36296778f9c01b847da81ad06308fb709c5
Enso Llanos <ensollanos@gmail.com>
2023-11-07 18:16:03 -0500

    Terminando sipper

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@ window.swiper = new Swiper ( {
     el: '.slider__contenedor',
     slideClass: 'slider__slide',
     createElements: true,
+    autoplay: {
+        delay: 5000
+    },
+    loop: true,
+    spaceBetween: 20,
+    pagination: true,
+    navigation: true,
 });
